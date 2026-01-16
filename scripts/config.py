c5450e663cda30514ed1c7747d5f64f3a7310f12
Dapeng Wei <tiwheeharniver@outlook.com>
2026-01-17 00:19:56 +0530

    update

---
+++ scripts/config.py
@@ -0,0 +1,17 @@
+##############################
+#     Paste API Key's Here
+##############################
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+try:
+    from scraper import system_check
+except:
+    pass
+
+clearbit_api_key = ""
+shodan_api_key = ""
+fullcontact_api_key = ""
+virus_total_api_key = ""
+email_hunter_api_key = ""
